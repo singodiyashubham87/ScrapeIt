51fdfae51e95618cabaa54c3d2bffd2fb267c81f
MisterMickey <singodiyapeechuparashubham@gmail.com>
2023-08-22 04:59:04 +0530

    intial setup done

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+function App() {
+  return (
+    <>
+      <h1 className='text-[4rem]'>ScrapeIt</h1>
+    </>
+  );
+}
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#00FCED',
+        secondary: '#D9D9D9',
+      },
+      fontFamily: {
+        primary: 'IBM Plex Mono',
+      }
+    },
+  },
+  plugins: [],
+}
+
