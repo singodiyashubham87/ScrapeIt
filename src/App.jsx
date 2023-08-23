7f4aa13bac9ebc6c08cfcfef7e6d2c6e854e21f8
MisterMickey <singodiyapeechuparashubham@gmail.com>
2023-08-23 10:23:31 +0530

    routes setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
+import Homepage from "./pages/Homepage";
 
 function App() {
-
   return (
-    <>
-    <LandingPage/>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" exact element={<LandingPage/>} />
+        <Route path="/homepage" element={<Homepage/>} />
+      </Routes>
+    </Router>
   );
 }
 export default App;
---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,7 @@
+function Homepage(){
+    return (
+        <h1>Homepage</h1>
+    );
+}
+
+export default Homepage;
