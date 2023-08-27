c0a9f041b800a47fafb0913f6ce5fb8a40f93bea
MisterMickey <singodiyapeechuparashubham@gmail.com>
2023-08-27 07:37:04 +0530

    patch: url validation done

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,22 +1,29 @@
+import { useState } from "react";
 import axios from "axios";
 import { useAuth0 } from "@auth0/auth0-react";
 import { Link } from "react-router-dom";
-import Loader from "./Loader"
-
+import Loader from "./Loader";
 
 function LandingPage() {
   const { loginWithRedirect, logout, isAuthenticated, isLoading, user } =
     useAuth0();
+  const [showModal, setShowModal] = useState(false);
+  const [alertError, setAlertError] = useState("");
 
-  // Click handler for Scrape button 
+  // Click handler for Scrape button
   const handleScrapeClick = async () => {
     const urlElement = document.getElementById("url").value;
     const radioElements = document.getElementsByName("contentType");
 
+    const urlRegex = /^(http[s]?:\/\/)(www\.)?[^\s$.?#].[^\s]*$/i;
 
-    if (urlElement === "") alert("Please enter a URL");
-
-    else {
+    if (urlElement === "") {
+      setAlertError("Please enter a URL");
+      setShowModal(true);
+    } else if (!urlRegex.test(urlElement)) {
+      setAlertError("Please enter a valid URL");
+      setShowModal(true);
+    } else {
       const apiKey = import.meta.env.VITE_API_NINJAS_X_API_KEY;
       const textOnly = radioElements[0].checked ? true : false;
       const url = `https://api.api-ninjas.com/v1/webscraper?url=${urlElement}&text_only=${textOnly}`;
@@ -27,14 +34,41 @@ function LandingPage() {
         })
         .catch((error) => console.warn(error));
 
-        // we can pass data through this way too but will become undefined on page reload 
-        // navigateTo = useNavigate();
-        // navigateTo("/homepage", { state: { data: res.data.data } }); 
-        sessionStorage.setItem("data", res.data.data);
+      // we can pass data through this way too but will become undefined on page reload
+      // navigateTo = useNavigate();
+      // navigateTo("/homepage", { state: { data: res.data.data } });
+      sessionStorage.setItem("data", res.data.data);
     }
   };
 
-  // Returns Loader component if authentication is in progress 
+  const closeModal = () => setShowModal(false);
+
+  const showModalComponent = () => {
+    return (
+      <>
+        <div
+          onClick={closeModal}
+          className="wrapper top-[0] bottom-[0] right-[0] left-[0] bg-[#272829] fixed opacity-[90%]"
+        ></div>
+        <div className="modalContainer w-[90%] vsm:w-[75%] sm:w-[60%] md:w-[auto] fixed top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]	 bg-black border-2 border-primary flex flex-col justify-center items-center px-[1rem] py-[1rem] md:px-[4rem] md:py-[2rem] xl:px-[5rem] xl:py-[3rem] ">
+          <h1 className="text-white text-[1.3rem] md:text-[1.7rem] xl:text-[2rem] text-yellow-500">
+            Alert!
+          </h1>
+          <p className="text-white text-center m-[1rem] md:m-[1.5rem] xl:m-[2rem] text-[1.3rem] xl:text-[2rem] md:text-[1.7rem]">
+            {alertError}
+          </p>
+            <button
+              onClick={closeModal}
+              className="close md:text-[1.5rem] text-white border border-primary hover:bg-primary hover:text-black px-[1rem] py-[.5rem] xl:px-[3rem] xl:py-[1rem]"
+            >
+              Close
+            </button>
+        </div>
+      </>
+    );
+  };
+
+  // Returns Loader component if authentication is in progress
   if (isLoading) {
     return <Loader />;
   }
@@ -107,13 +141,23 @@ function LandingPage() {
                 </label>
               </div>
             </div>
-            <Link
-              onClick={handleScrapeClick}
-              to="/homepage"
-              className="bg-transparent text-[1.2rem] vsm:text-[1.5rem] md:text-[2rem] text-primary px-16 hover:bg-primary hover:text-black border-2 border-primary"
-            >
-              Scrape
-            </Link>
+            {showModal ? (
+              <button
+                onClick={handleScrapeClick}
+                className="bg-transparent text-[1.2rem] vsm:text-[1.5rem] md:text-[2rem] text-primary px-16 hover:bg-primary hover:text-black border-2 border-primary"
+              >
+                Scrape
+              </button>
+            ) : (
+              <Link
+                onClick={handleScrapeClick}
+                to="/homepage"
+                className="bg-transparent text-[1.2rem] vsm:text-[1.5rem] md:text-[2rem] text-primary px-16 hover:bg-primary hover:text-black border-2 border-primary"
+              >
+                Scrape
+              </Link>
+            )}
+            {showModal && showModalComponent()}
           </div>
           <h3 className="text-secondary text-[1rem] vsm:text-[1.2rem] md:text-[2rem] sm:text-[1.5rem]">
             Made with <span>&#x2764;</span> by Mister Mickey
