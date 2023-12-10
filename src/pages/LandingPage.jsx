4838175281a981a74a678c5f805caedec493ef9b
ayann07 <raza.ayan2002@gmail.com>
2023-12-10 16:55:49 +0530

    Add light mode to the app

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -19,7 +19,7 @@ function LandingPage() {
   const [alertError, setAlertError] = useState(""); //alert message in modal component
   const [loader, setLoader] = useState(false); //loader variable
   const [selectedRadioButton, setSelectedRadioButton] = useState(false);
-
+  const [darkMode, setDarkMode] = useState(true);
   // Navigation Hook
   const navigateTo = useNavigate();
 
@@ -56,7 +56,7 @@ function LandingPage() {
         const textOnly = radioElements[0].checked ? true : false; // Check if user want text only response or HTML
         const url = `https://api.api-ninjas.com/v1/webscraper?url=${urlElement}&text_only=${textOnly}`;
 
-         // API request using axios
+        // API request using axios
         const res = await axios
           .get(url, {
             headers: { "X-Api-Key": apiKey },
@@ -86,18 +86,18 @@ function LandingPage() {
       }
     }
   };
- // Function to get the current time and determine the greeting
-const getGreeting = () => {
-  const currentHour = new Date().getHours();
+  // Function to get the current time and determine the greeting
+  const getGreeting = () => {
+    const currentHour = new Date().getHours();
 
-  if (currentHour >= 5 && currentHour < 12) {
-    return 'Good Morning';
-  } else if (currentHour >= 12 && currentHour < 18) {
-    return 'Good Afternoon';
-  } else {
-    return 'Good Evening';
-  }
-};
+    if (currentHour >= 5 && currentHour < 12) {
+      return 'Good Morning';
+    } else if (currentHour >= 12 && currentHour < 18) {
+      return 'Good Afternoon';
+    } else {
+      return 'Good Evening';
+    }
+  };
 
 
   // Modal and loader handling functions
@@ -111,7 +111,7 @@ const getGreeting = () => {
           onClick={closeModal}
           className="wrapper top-[0] bottom-[0] right-[0] left-[0] bg-[#272829] fixed opacity-[90%]"
         ></div>
-        <div className="modalContainer w-[90%] vsm:w-[75%] sm:w-[60%] md:w-[auto] fixed top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] bg-black border-2 border-primary flex flex-col justify-center items-center px-[1rem] py-[1rem] md:px-[4rem] md:py-[2rem] xl:px-[5rem] xl:py-[3rem] ">
+        <div className="modalContainer w-[90%] vsm:w-[75%] sm:w-[60%] md:w-[auto] fixed top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] bg-black border-2 border-primary flex flex-col justify-center items-center px-[1rem] py-[1rem] md:px-[4rem] md:py-[2rem] xl:px-[5rem] xl:py-[3rem]">
           <h1 className="text-white text-[1.3rem] md:text-[1.7rem] xl:text-[2rem] text-yellow-500">
             {alert || "Alert!"}
           </h1>
@@ -150,31 +150,37 @@ const getGreeting = () => {
 
   return (
     <>
-      <div className="landingContainer bg-black w-[100%] h-[100vh] font-primary relative">
+      <div className={`landingContainer ${darkMode ? 'dark' : 'light'} bg-black w-[100%] h-[100vh] font-primary relative`}>
         <div className="content h-[100vh] flex flex-col justify-evenly items-center border-2 border-white-700">
+          <button
+              onClick={() => setDarkMode(!darkMode)}
+              className={`toggleButton bg-transparent text-white px-4 py-2 border-2 border-white fixed top-0 left-0 m-4 ${darkMode ? 'border-white' : 'border-black'}`}
+            >
+              {darkMode ? 'Light Mode' : 'Dark Mode'}
+            </button>
           <Link
             to={"https://github.com/singodiyashubham87/ScrapeIt"}
             className="absolute top-0 right-0 p-6"
             target="_blank"  // open link in a new tab
           >
-            <img src={ghlogo} alt="small_github_logo" className="h-10 ghlogo-vsm:h-[2.5rem] ghlogo-vvsm:h-[2rem]"/>
+            <img src={ghlogo} alt="small_github_logo" className="h-10 ghlogo-vsm:h-[2.5rem] ghlogo-vvsm:h-[2rem]" />
           </Link>
           <div className="auth text-center">
             {isAuthenticated && (
               <h1 className="greeting text-secondary text-[1rem] vvsm:text-[1.5rem] vsm:text-[1.7rem] md:text-[2.5rem] sm:text-[2rem] mb-[1rem]">
-               {getGreeting()}, {user.name}!
+                {getGreeting()}, {user.name}!
               </h1>
             )}
             {!isAuthenticated ? (
               <button
-                className="bg-black text-white text-[1.5rem] sm:text-[2rem] px-[2rem] sm:px-[4rem] hover:bg-white hover:text-black border-2 border-white-700"
+                className={`bg-black text-white text-[1.5rem] sm:text-[2rem] px-[2rem] sm:px-[4rem] hover:bg-white hover:text-black border-2 border-white-700 ${darkMode ? 'border-loginwhite' : 'border-loginblack'}`}
                 onClick={() => loginWithRedirect()}
               >
                 Log In
               </button>
             ) : (
               <button
-                className="bg-black text-white text-[1.5rem] sm:text-[2rem] px-[2rem] sm:px-[4rem] hover:bg-white hover:text-black border-2 border-white-700"
+                className={`bg-black text-white text-[1.5rem] sm:text-[2rem] px-[2rem] sm:px-[4rem] hover:bg-white hover:text-black border-2 border-white-700 ${darkMode ? 'border-loginwhite' : 'border-loginblack'}`}
                 onClick={() =>
                   logout({ logoutParams: { returnTo: window.location.origin } })
                 }
@@ -184,7 +190,7 @@ const getGreeting = () => {
             )}
           </div>
 
-          <div className="inputBox h-[25%] md:h-[30%] w-[90%] sm:w-[70%] md:w-[60%] lg:w-[50%] xl:w-[35%] 2xl:w-[30%] flex flex-col items-center justify-between border-2 p-4 border-white">
+          <div className={`inputBox h-[25%] md:h-[30%] w-[90%] sm:w-[70%] md:w-[60%] lg:w-[50%] xl:w-[35%] 2xl:w-[30%] flex flex-col items-center justify-between border-2 p-4 border-white ${darkMode ? 'border-w' : 'border-b'}`}>
             <input
               id="url"
               className="border-none w-[100%] text-center text-[1rem] vsm:text-[1.3rem] md:text-[1.5rem] xl:text-[2rem] px-2 py-1"
